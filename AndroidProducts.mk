de0560483693326ba4096ffe8dbf2522de980b92
Debayan Kar <kardebayan3@gmail.com>
2021-12-26 04:22:52 +0000

    G: ArrowOS bringup
    Change-Id: Ib3bdb9356cc6930f8cd9e4441d7b48bad07e2643

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2022 The ArrowOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_G.mk
+    $(LOCAL_DIR)/arrow_G.mk
